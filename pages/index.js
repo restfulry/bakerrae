7da6fc7813f40e5a6c884ab64685272ba9df4034
Ry <69879790+ryanjjlee@users.noreply.github.com>
2021-07-19 18:01:10 -0400

    - add form submission handler for Stripe - change initial data fetch to getServerSideProps for a fetch on each request

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,45 @@
 import { server } from "../config";
-import Head from "next/head";
-import Image from "next/image";
+import { useRouter } from "next/router";
+import { useState } from "react";
 import BakeryDropList from "../components/BakeryDropList";
 
 export default function Home({ data }) {
+  const [orderDetails, setOrderDetails] = useState({});
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("going to checkout");
+
+    const res = await fetch(`${server}/orders/checkout`, {
+      method: "POST",
+      body: JSON.stringify(orderDetails),
+    });
+
+    const url = await res.text();
+    router.push(url);
+  };
+
   return (
     <div>
       <BakeryDropList data={data} />
+      <form onSubmit={handleSubmit}>
+        <button type="submit">Checkout</button>
+      </form>
     </div>
   );
 }
 
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
   const res = await fetch(`${server}/bakerydrops`);
   const data = await res.json();
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
 
   return {
     props: {
