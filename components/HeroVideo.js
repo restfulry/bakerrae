c65dfe6060982328314adbd3f2f20a293d6628f6
Ryan Lee <ryan@RY-MBP-TB-Local>
2021-09-08 12:11:43 -0400

    styling

--- components/HeroVideo.js
+++ components/HeroVideo.js
@@ -2,18 +2,22 @@ import HeroVideoStyles from "../styles/HeroVideoStyles.module.css";
 
 const HeroVideo = () => {
   return (
-    <div className="sectionOrange">
-      <div className="container">
+    <div className="container">
+      <div className={HeroVideoStyles.header}>
+        <h1>As seen on BlogTO</h1>
+        {/* <Image src={"/line.svg"} width="70" height="10" /> */}
+      </div>
+      <div className="sectionOrange">
         <div className={HeroVideoStyles.video}>
           <iframe
             width="560"
             height="315"
             src="https://www.youtube.com/embed/UtPprUWteeo"
             title="Baker Rae x Cafe"
-            frameborder="0"
+            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; modestbranding;"
-            allowfullscreen
-          ></iframe>
+            allowFullScreen
+          />
         </div>
       </div>
     </div>
--- components/InfoSection.js
+++ components/InfoSection.js
@@ -6,16 +6,31 @@ const InfoSection = () => {
   return (
     <div className="container">
       <div className="sectionOrange">
-        <div className={InfoSectionStyles.image}>
-          <Image
-            src={"/20200927_A7S04418_004-min.jpg"}
-            width="300"
-            height="400"
-          />
+        <div className={InfoSectionStyles.left}>
+          <div className={InfoSectionStyles.image1}>
+            <Image
+              src={"/20200927_A7S04418_004-min.jpg"}
+              width="300"
+              height="400"
+            />
+          </div>
         </div>
-        <div className={InfoSectionStyles.text}>
-          <h1>Hand made baked goods & cold brew concentrate.</h1>
-          <h3>Bakery drops every two weeks.</h3>
+        <div className={InfoSectionStyles.right}>
+          <div className={InfoSectionStyles.text}>
+            <h1>Hand made baked goods & cold brew concentrate.</h1>
+            <h3>Bakery drops every two weeks.</h3>
+          </div>
+          <div className={InfoSectionStyles.circle}>
+
+            <Image src={"/blurredcircle_orange.png"} width="500" height="500" />
+          </div>
+          <div className={InfoSectionStyles.image2}>
+            <Image
+              src={"/20200927_A7S04418_004-min.jpg"}
+              width="300"
+              height="400"
+            />
+          </div>
         </div>
       </div>
     </div>
