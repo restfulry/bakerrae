44bb311ab6cdf8e373da6a02cdc0ea14dcae4d6c
Ry <69879790+ryanjjlee@users.noreply.github.com>
2021-10-15 23:10:43 -0400

    move faq content to utils folder

--- pages/index.js
+++ pages/index.js
@@ -13,30 +13,14 @@ import CheckoutButton from "../components/CheckoutButton";
 import Accordion from "../components/Accordion";
 import Footer from "../components/Footer";
 
+import { faqData } from "../utils/content";
+
 export default function Home({ data }) {
   const router = useRouter();
 
   const [cart, setCart] = useState([]);
   const [shipping, setShipping] = useState("pickup");
   const [disabled, setDisabled] = useState(true);
-
-  const faqData = [
-    {
-      question: "How long do your cinnamon buns keep?",
-      answer:
-        "We recommend you eat your buns as soon as possible for the best quality. However, you can keep them in an airtight container for up to 2 weeks. To reheat, microwave for 30s - 1min until warm.",
-    },
-    {
-      question: "Are your cinnamon buns nut-free?",
-      answer:
-        "We currently do not use any nuts in our recipe, however our commercial kitchen may not be nut-free.",
-    },
-    {
-      question: "Do the buns contain dairy or eggs?",
-      answer:
-        "The doughs contain dairy, however they do not contain eggs. Some fillings or toppings may be made with egg such as our kaya.",
-    },
-  ];
 
   const itemInCart = (product) => {
     let itemInCart = cart.find((item) => item.product.id === product.id);
---
+++ utils/content.js
@@ -0,0 +1,17 @@
+export const faqData = [
+  {
+    question: "How long do your cinnamon buns keep?",
+    answer:
+      "We recommend you eat your buns as soon as possible for the best quality. However, you can keep them in an airtight container for up to 2 weeks. To reheat, microwave for 30s - 1min until warm.",
+  },
+  {
+    question: "Are your cinnamon buns nut-free?",
+    answer:
+      "We currently do not use any nuts in our recipe, however our commercial kitchen may not be nut-free.",
+  },
+  {
+    question: "Do the buns contain dairy or eggs?",
+    answer:
+      "The doughs contain dairy, however they do not contain eggs. Some fillings or toppings may be made with egg such as our kaya.",
+  },
+];
