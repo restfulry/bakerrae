5fbf7f66acf290eefc8eb9065812debb01f0e0a0
Ryan Lee <ryan@RY-MBP-TB-Local>
2021-12-26 21:53:01 -0800

    import faq line svg

--- components/FaqComponent.js
+++ components/FaqComponent.js
@@ -1,6 +1,8 @@
 import Accordion from "./Accordion";
 import Image from "next/image";
 
+import linePic from '../public/line.svg';
+
 import FaqStyles from "../styles/FaqStyles.module.css";
 
 const FaqComponent = ({ faqData }) => {
@@ -9,7 +11,7 @@ const FaqComponent = ({ faqData }) => {
       <div className={FaqStyles.faqWrapper}>
         <div className={FaqStyles.subHeader}>
           <h1>General FAQs</h1>
-          <Image src={"/line.svg"} width="70" height="10" />
+          <Image src={linePic} width="70" height="10" />
         </div>
         <div className={FaqStyles.faqAccordion} key="faqAccordion">
           {faqData.map(({ question, answer, idx }) => (
