c6fe6f371f8a0657191353f684b4b72e259c5c9b
Ryan Lee <ryan@RY-MBP-TB-Local>
2022-02-27 12:08:39 -0500

    config index js

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 const dev = process.env.NODE_ENV !== "production";
 
 console.log('SERVER', process.env.NEXT_PUBLIC_API);
-export const server = process.env.NEXT_PUBLIC_API;
+
+export const server = dev ? "http://localhost:1337" : process.env.NEXT_PUBLIC_API;
