7668b145ede87f0eba472be65d0cfbeb32475b79
Ryan Lee <ryan@RY-MBP-TB-Local>
2022-02-27 15:09:42 -0500

    domain switch in next config js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
+const dev = process.env.NODE_ENV !== "production";
+
+const domain = dev ? "localhost" : "api.bakerrae.com";
+
 module.exports = {
   images: {
-    domains: ["localhost"],
+    domains: [domain],
   },
 };
